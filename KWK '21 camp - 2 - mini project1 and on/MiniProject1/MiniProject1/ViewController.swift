2142ad97cb4b713a9a7a4f73d54e2824969f5bce
Sofia Ocando <88101688+sofiaocando@users.noreply.github.com>
2021-07-29 01:46:05 -0400

    Add files via upload
    mini project 1 - 07/29

---
+++ KWK '21 camp - 2 - mini project1 and on/MiniProject1/MiniProject1/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MiniProject1
+//
+//  Created by Scholar on 7/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var titleLabel: UIView!
+    @IBOutlet var factsLabel: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func factButton(_ sender: Any) {
+    }
+    
+
+}
+
