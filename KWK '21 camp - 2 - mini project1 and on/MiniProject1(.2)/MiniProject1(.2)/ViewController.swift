54d9d4c29f51c9f28ee834c86237fc5c50001ca9
Sofia Ocando <88101688+sofiaocando@users.noreply.github.com>
2021-07-29 14:16:34 -0400

    Add files via upload
    includes start of mini project 2

---
+++ KWK '21 camp - 2 - mini project1 and on/MiniProject1(.2)/MiniProject1(.2)/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MiniProject1(.2)
+//
+//  Created by Scholar on 7/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var appTitle: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
